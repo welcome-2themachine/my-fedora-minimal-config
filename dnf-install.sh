bfbce189dbbaff59a6822598d8322d85ad42d5ed
Welcome2-theMachine <welcome2.themachine411@gmail.com>
2022-12-12 22:22:55 -1000

    initial commit

--- dnf-install.sh
+++ dnf-install.sh
@@ -8,7 +8,11 @@ sudo dnf config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo
 
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
 sudo dnf update
+dnf check-update
 
 sudo dnf install /
 nano /
@@ -53,4 +57,5 @@ libreoffice /
 virtualbox /
 firefox /
 nmap /
+code /
 cowsay
