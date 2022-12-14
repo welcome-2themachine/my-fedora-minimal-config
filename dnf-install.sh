aae272fa6a94174f126fb7f82cf6257d1818db42
Welcome2-theMachine <welcome2.themachine411@gmail.com>
2022-12-13 20:21:32 -1000

    fixed some of the flatpak errors (hopefully)

--- dnf-install.sh
+++ dnf-install.sh
@@ -10,6 +10,7 @@ sudo dnf config-manager \
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
+sudo dnf install flatpak
 flatpak remote-add --if-not-exists \
 flathub https://flathub.org/repo/flathub.flatpakrepo
 
