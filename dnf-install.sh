d8058037da0ba57d52b063b6d4766e1425c95e08
Welcome2-theMachine <welcome2.themachine411@gmail.com>
2022-12-27 20:55:24 -1000

    added rpm-fusion mirrors

--- dnf-install.sh
+++ dnf-install.sh
@@ -10,6 +10,8 @@ sudo dnf config-manager \
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
 sudo dnf update
 dnf check-update
 
