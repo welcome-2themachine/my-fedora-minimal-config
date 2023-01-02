a65fe49b0d16057c08da89cb868b6c500b7acecf
welcome-2themachine <welcome2.themachine411@gmail.com>
2023-01-01 21:05:47 -1000

    fixed flatpack installs

--- flatpak-install.sh
+++ flatpak-install.sh
@@ -10,7 +10,7 @@ org.signal.Signal \
 org.videolan.VLC \
 com.brave.Browser \
 com.parsecgaming.parsec \
-#com.google.Chrome \ borked for some reason
+com.google.Chrome \
 com.valvesoftware.Steam \
 org.libreoffice.LibreOffice
 
