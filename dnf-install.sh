8bd7897bda9bbe2736839696505b27ef0e8d2ff9
welcome-2themachine <welcome2.themachine411@gmail.com>
2023-01-02 17:01:51 -1000

    moved the free/nonfree repos to dnf

--- dnf-install.sh
+++ dnf-install.sh
@@ -9,6 +9,8 @@ sudo dnf config-manager \
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpmS
 
 sudo dnf update
 dnf check-update
--- nvidia-drivers.sh
+++ nvidia-drivers.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
 sudo dnf update
 
 sudo dnf install \
