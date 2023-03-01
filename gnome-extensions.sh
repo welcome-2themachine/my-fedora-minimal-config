f89cd8b6e4da83113f6e6ed462070e4afa6526aa
welcome-2themachine <welcome2.themachine411@gmail.com>
2023-02-28 22:24:44 -1000

    added a new file to install the shell extensions I use daily

---
+++ gnome-extensions.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+git clone https://github.com/brunelli/gnome-shell-extension-installer
+
+chmod +x $(pwd)/gnome-shell-extension-installer/gnome-shell-extension-installer
+sudo mv $(pwd)/gnome-shell-extension-installer/gnome-shell-extension-installer /usr/bin/
+
+gnome-shell-extension-installer 779 307 4158 18 4506 3733 19 97 1714 5344 --yes
+
+#779 - Clipboard Indicator
+#307 - Dash to Dock
+#4158 - Gnome 4x UI Improvements
+#1732 - GTK Title Bar
+#18 - Native Window Placement
+#4506 - Simple System Monitor
+#3733 - Tiling Assitant
+#19 - User Themes
+#97 - Coverflow Alt-Tab
+#1714 - SSH Search Provider Reborn
+#5344 - super graphics control
