82dab0d3412caf57c68c7f2c8f75a3dcacbfd532
welcomw <welcome2.themachine411@gmail.com>
2023-05-24 20:45:09 -1000

    added caffine

--- gnome-extensions.sh
+++ gnome-extensions.sh
@@ -5,7 +5,7 @@ git clone https://github.com/brunelli/gnome-shell-extension-installer
 chmod +x $(pwd)/gnome-shell-extension-installer/gnome-shell-extension-installer
 sudo mv $(pwd)/gnome-shell-extension-installer/gnome-shell-extension-installer /usr/bin/
 
-gnome-shell-extension-installer 779 307 4158 18 4506 3733 19 97 1714 5344 1732 --yes
+gnome-shell-extension-installer 779 307 4158 18 4506 3733 19 97 1714 5344 1732 517 --yes
 
 #779 - Clipboard Indicator
 #307 - Dash to Dock
@@ -18,3 +18,4 @@ gnome-shell-extension-installer 779 307 4158 18 4506 3733 19 97 1714 5344 1732 -
 #97 - Coverflow Alt-Tab
 #1714 - SSH Search Provider Reborn
 #5344 - super graphics control
+#517 - caffine
